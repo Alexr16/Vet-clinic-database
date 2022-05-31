8abecd829f3c0e0614503cefdc44385c07e141d5
Alexr16 <j.alex-1996@hotmail.com>
2022-05-31 13:38:02 -0500

    Set species to digimon or pokemon

--- queries.sql
+++ queries.sql
@@ -10,3 +10,8 @@ SELECT * FROM animals WHERE weight_kg BETWEEN '10.4' AND '17.3';
 BEGIN;
 UPDATE animals SET species = 'unspecified';
 ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
